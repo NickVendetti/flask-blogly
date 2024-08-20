ecdcabd382e3a47f4cbaa3dfb8fe2df063ca665c
nickvendetti <nrvendetti@gmail.com>
2024-08-19 23:07:00 -0400

    Part Three Many to Many

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
 class User(db.Model):
     """Site user."""
 
@@ -14,24 +15,41 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
+    posts = db.relationship('Post', backref='user', cascade="all, delete-orphan")
+
     @property
     def full_name(self):
         """Return full name of user."""
         return f"{self.first_name} {self.last_name}"
-    
+
 class Post(db.Model):
     """Blog post."""
     
-    __tablename__ = "posts"  # This should match your table name
+    __tablename__ = "posts"
 
-    id = db.Column(db.Integer, primary_key=True)  # Primary key definition
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    user = db.relationship('User', backref='posts')
+    tags = db.relationship('Tag', secondary='post_tags', backref='posts')
 
+class Tag(db.Model):
+    """Tag for categorizing posts."""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+class PostTag(db.Model):
+    """Join table between Post and Tag."""
+
+    __tablename__ = "post_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
 
 def connect_db(app):
     """Connect this database to provided Flask app."""
